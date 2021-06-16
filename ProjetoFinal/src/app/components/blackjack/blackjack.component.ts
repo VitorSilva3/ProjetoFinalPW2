c35788ac0d2a03fd6f483f13931e10b727e4e28f
Xavier Alves Monteiro <75092505+xavier-monteiro@users.noreply.github.com>
2021-06-16 20:40:34 +0100

    blackjack 85% feito
    resolvi muitos bugs e criei outros e resolvi-os tb,
    so vai ate 4 cartas eu sei que blackjack o maximo é 5 mas a probabilidade de isso acontecer é super baixa e ia dar-me uma dor de cabeça fdd
    ja pus +- responsivo
    ja pus as apostas a dar
    falta por responsivo para o tablet e melhorar nos outros , ta meio a javardo mas come-se

--- ProjetoFinal/src/app/components/blackjack/blackjack.component.ts
+++ ProjetoFinal/src/app/components/blackjack/blackjack.component.ts
@@ -17,6 +17,14 @@ export class BlackjackComponent implements OnInit {
   @ViewChild('dealerCards2') dCard2: ElementRef;
   @ViewChild('dealerCards3') dCard3: ElementRef;
   @ViewChild('dealerCards4') dCard4: ElementRef;
+  @ViewChild('clearbtn') clearButton: ElementRef;
+  @ViewChild('dealbtn') dealButton: ElementRef;
+  @ViewChild('hitbtn') hitButton: ElementRef;
+  @ViewChild('standbtn') standButton: ElementRef;
+
+
+
+
 
   Decks : CartasService;
   constructor(private cartasService : CartasService) {
@@ -24,6 +32,8 @@ export class BlackjackComponent implements OnInit {
    }
 
   ngOnInit(): void {
+
+
   }
 
   valor_apostado : number = 0;
@@ -55,6 +65,8 @@ export class BlackjackComponent implements OnInit {
   pontosPlayer : number =0;
   pontosDealer : number =0;
 
+  canBet = true;
+
 
   bet(valor : number)
   {
@@ -62,6 +74,10 @@ export class BlackjackComponent implements OnInit {
     return;
 
 
+    if(this.canBet ==false)
+    {
+      return alert("o tempo para apostar ja acabou");
+    }
       this.valor_apostado += valor;
       console.log('Apostou : '+valor);
       console.log('Valor da aposta: '+this.valor_apostado);
@@ -69,44 +85,63 @@ export class BlackjackComponent implements OnInit {
   clearBet()
   {
     this.valor_apostado =0;
+
   }
 
   deal()
   {
-      this.Decks.getCards().subscribe(data => {this.resultado=data;console.log(this.resultado);this.criarCartas(this.resultado);this.organize()});
-
+    console.log('clickei aqui')
+    if(this.valor_apostado==0)
+    {
+      return alert("aposta alguma coisa");
+    }
+    this.canBet = false;
+      this.Decks.getCards().subscribe(data => {this.resultado=data;console.log(this.resultado);this.validarNCartas(this.resultado);this.criarCartas(this.resultado);this.organize()});
+      this.hitButton.nativeElement.style.opacity = 100;
+      this.standButton.nativeElement.style.opacity = 100;
+      this.clearButton.nativeElement.style.opacity = 0;
   }
 
-
-
-  criarCartas(resultado : any)
+  validarNCartas(resultado:any)
   {
+      if(Number(resultado.remaining)<=20)
+      {
+        this.Decks.shuffle().subscribe(data =>{console.log(data)});
+      }
+  }
 
-    if (this.contar==0)
+  criarCartas(resultado : any, who?:string)
+  {
+    console.log(this.contar);
+    console.log("Contar dealer:" +this.contarDealer);
+
+    if(who!="dealer")
     {
-      this.dCarta1 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
-      this.dCarta2 = new Carta(resultado.cards[1].value,resultado.cards[1].suit,resultado.cards[1].image,this.validarPontos(resultado.cards[1].value));
-      this.pCarta1 = new Carta(resultado.cards[2].value,resultado.cards[2].suit,resultado.cards[2].image,this.validarPontos(resultado.cards[2].value));
-      this.pCarta2 = new Carta(resultado.cards[3].value,resultado.cards[3].suit,resultado.cards[3].image,this.validarPontos(resultado.cards[3].value));
+      if (this.contar==0)
+      {
+        this.dCarta1 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
+        this.dCarta2 = new Carta(resultado.cards[1].value,resultado.cards[1].suit,resultado.cards[1].image,this.validarPontos(resultado.cards[1].value));
+        this.pCarta1 = new Carta(resultado.cards[2].value,resultado.cards[2].suit,resultado.cards[2].image,this.validarPontos(resultado.cards[2].value));
+        this.pCarta2 = new Carta(resultado.cards[3].value,resultado.cards[3].suit,resultado.cards[3].image,this.validarPontos(resultado.cards[3].value));
 
-      this.dealerCards.push(this.dCarta1,this.dCarta2);
-      this.playerCards.push(this.pCarta1,this.pCarta2);
+        this.dealerCards.push(this.dCarta1,this.dCarta2);
+        this.playerCards.push(this.pCarta1,this.pCarta2);
 
 
-    } else if(this.contar ==1)
-    {
-      this.pCarta3 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
-      this.playerCards.push(this.pCarta3);
+      } else if(this.contar ==1)
+      {
+        this.pCarta3 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
+        this.playerCards.push(this.pCarta3);
 
-    }else if(this.contar ==2)
-    {
-      this.pCarta4 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
-      this.playerCards.push(this.pCarta4);
+      }else if(this.contar ==2)
+      {
+        this.pCarta4 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
+        this.playerCards.push(this.pCarta4);
 
+      }
     }
-
-
-    if(this.contarDealer==1)
+    else{
+      if(this.contarDealer==1)
     {
       this.dCarta3 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
       this.dealerCards.push(this.dCarta3);
@@ -116,6 +151,12 @@ export class BlackjackComponent implements OnInit {
       this.dCarta4 = new Carta(resultado.cards[0].value,resultado.cards[0].suit,resultado.cards[0].image,this.validarPontos(resultado.cards[0].value));
       this.dealerCards.push(this.dCarta4);
     }
+
+    }
+
+
+
+
 
   }
 
@@ -130,15 +171,15 @@ export class BlackjackComponent implements OnInit {
 
         console.log(this.dealerCards);
         console.log(this.playerCards);
-        this.contar = 1;
+
       } else if (this.contar == 1)
       {
         this.pCard3.nativeElement.src = this.playerCards[2].imagem_url;
-        this.contar = 2;
+
       }else if (this.contar == 2)
       {
         this.pCard4.nativeElement.src = this.playerCards[3].imagem_url;
-        this.contar = 3;
+
       }
 
 
@@ -158,20 +199,35 @@ export class BlackjackComponent implements OnInit {
     return pontos;
   }
 
-  hit()
+  resolverBug()
   {
-    this.Decks.hit().subscribe(data => {this.resultado=data;console.log(this.resultado);this.criarCartas(this.resultado);this.organize();this.PodeContinuar()});
+    this.hit("player");
+  }
+  hit(who? : string )
+  {
+    if(who=="player")
+    {
+      this.contar+=1;
+      this.Decks.hit().subscribe(data => {this.resultado=data;console.log(this.resultado);this.criarCartas(this.resultado,"player");this.organize();this.PodeContinuar()});
+
+    }else if(who=="dealer")
+    {
+      this.contarDealer+=1;
+      this.Decks.hit().subscribe(data => {this.resultado=data;console.log(this.resultado);this.criarCartas(this.resultado,"dealer");this.organize();this.PodeContinuar()});
+    }
+
   }
 
   PodeContinuar()
   {
 
       this.pontosPlayer = this.contarPontos(this.playerCards);
+      this.pontosDealer = this.contarPontos(this.dealerCards);
 
       if(this.pontosPlayer>21)
         {
           console.log("perdeu");
-          this.EndGame("p-lost");
+          this.EndGame("Parrebenta");
         }
 
       else if (this.contarDealer ==1)
@@ -181,7 +237,7 @@ export class BlackjackComponent implements OnInit {
           if (this.pontosDealer>21)
           {
             console.log("Player ganha");
-            this.EndGame("p-win");
+            this.EndGame("Darrebenta");
           } else if(this.pontosDealer>this.pontosPlayer)
           {
             console.log("Dealer ganhou");
@@ -192,8 +248,7 @@ export class BlackjackComponent implements OnInit {
             this.EndGame("draw");
           } else {
             console.log("tira outra carta");
-            this.contarDealer = 2;
-            this.hit();
+            this.hit("dealer");
           }
         }
 
@@ -202,7 +257,7 @@ export class BlackjackComponent implements OnInit {
         if (this.pontosDealer>21)
         {
           console.log("Player ganha");
-          this.EndGame("p-win");
+          this.EndGame("Darrebentou");
         } else if(this.pontosDealer>this.pontosPlayer)
         {
           console.log("Dealer ganhou");
@@ -262,8 +317,7 @@ export class BlackjackComponent implements OnInit {
     } else if(this.pontosDealer== this.pontosPlayer || this.pontosDealer<this.pontosPlayer)
     {
       console.log('dealer precisa tirar mais uma carta');
-      this.contarDealer = 1;
-      this.hit();
+      this.hit("dealer");
     }
 
   }
@@ -272,17 +326,7 @@ export class BlackjackComponent implements OnInit {
 
   EndGame(result : string)
   {
-      if(result == "p-win")
-      {
-        console.log("Player ganhou")
-      } else if (result == "p-lost"){
 
-        console.log("Player perdeu");
-
-      } else if (result == "draw")
-      {
-        console.log("Empate");
-      }
 
 
       console.log("Pontos dealer: "+this.pontosDealer);
@@ -297,12 +341,37 @@ export class BlackjackComponent implements OnInit {
       }
       if (this.dealerCards.length ==3)
       {
+        this.dCard2.nativeElement.src = this.dealerCards[1].imagem_url;
         this.dCard3.nativeElement.src = this.dealerCards[2].imagem_url;
       }
 
       if (this.dealerCards.length ==4)
       {
+        this.dCard2.nativeElement.src = this.dealerCards[1].imagem_url;
+        this.dCard3.nativeElement.src = this.dealerCards[2].imagem_url;
         this.dCard4.nativeElement.src = this.dealerCards[3].imagem_url;
+      }
+
+      if(result == "p-win")
+      {
+        console.log("Player ganhou")
+
+        this.dinheiro_user += this.valor_apostado;
+        return alert(`O player ganhou com ${this.pontosPlayer} pontos.`);
+      } else if (result == "p-lost"){
+
+        this.dinheiro_user -= this.valor_apostado;
+        return alert(`O dealer ganhou com ${this.pontosDealer} pontos.`);
+
+      } else if (result == "draw")
+      {
+        return alert(`Que belo empate`);
+      } else if (result == "Darrebenta")
+      {
+        return alert(`O dealer arrebentou com ${this.pontosDealer} pontos.`);
+      }else if (result == "Parrebenta")
+      {
+        return alert(`O player arrebentou com ${this.pontosPlayer} pontos.`);
       }
 
 
